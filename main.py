9dfdfa68688ab0d4c5e0f8fcd2e84d520c2c8bb1
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2020-12-29 18:09:18 +0600

    WORK: working on the flask app

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from flask import Flask, render_template      
 import pandas as pd 
 import os 
 import plotly.express as px
 import datetime
 
-def main(): 
-	final_csv = [] 
-	entries = os.listdir("Data/csv/")
-	for i in entries: 
-		csv_path = os.path.join("Data/csv/", i ) #define the path for the csv files
-		name, extension = os.path.splitext(csv_path) #check if csv file
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    data = readData()
+    categoricalDaysExpenditure = categoricalDaysExpenditure(data)
+    return render_template('home.html', plot= categoricalDaysExpenditure)
+    # return render_template("home.html")
+
+def readData():
+	final_csv = [] 
+	entries = os.listdir("./static/data/csv/")
+	for i in entries: 
+		name, extension = os.path.splitext(csv_path) #check if csv file
 		if (extension == ".csv"): 
+            csv_path = os.path.join("./static/data/csv/", i ) #define the path for the csv files
 			solo_csv = pd.read_csv(csv_path,index_col=False, header=None, engine='python') # return a panda data frame, engine=python required for unicode issue
 			solo_csv[6] = solo_csv[6].str.replace('DR','').str.replace(",", '') #remove the string DR from DEBIT
 			solo_csv[7] = solo_csv[7].str.replace('CR','').str.replace(",", '') #remove the string CR from CREDIT
@@ -20,46 +29,31 @@ def main():
 		else: 
 			print("file {} not a csv ".format(csv_path))
 
+		return createDataFrame(final_csv)
+	
+def createDataFrame(final_csv): 
 	data = pd.concat(final_csv, axis=0, sort=False) 
 	data.columns = ["date", "journal", "action", "to", "category", "item", "debit", "credit", "balance"] #set the column headers
 	data["debit"] = data.debit.astype('float64')
 	data["credit"] = data.credit.astype('float64')
 	data["balance"] = data.balance.astype('float64')
 	data["date"] = pd.to_datetime(data['date'])
-	data['day_of_week'] = data['date'].dt.day_name()
+	data['day'] = data['date'].dt.day_name()
 	data["date"] = pd.to_datetime(data['date']).dt.date
 	data = data.sort_values(by=['date'])
 	data.to_csv("Data/final/final_data.csv") #write the appended data to a csv file.
-
-	
 	print("----------------------\n" , data.dtypes, "\n----------------------")
 
-	dailyExpenditure(data)
-	categoricalExpenditure(data)
+	return data
 
-def dailyExpenditure(data):	
-	start_date , end_date = get_dates()
-	df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
-	fig = px.scatter(df, x="date", y="debit", labels={'debit':'Expenditure'}, hover_data=['category', 'item', 'date', 'debit', 'balance'])
-	fig.show()
 
-def categoricalExpenditure(data): 
-	start_date , end_date = get_dates()
-	df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
-	fig = px.bar(df, x="category", y="debit", labels={'debit':'Expenditure'}, hover_data=['category', 'item', 'date', 'debit', 'balance'])
-	fig.show()
-
-# def categoricalDaysExpenditure(data):
-# 	start_date , end_date = get_dates()
-# 	df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
-# 	fig = px.bar(df, x="sex", y="total_bill", color="smoker", barmode="group",
-#              facet_row="time", facet_col="day",
-#              category_orders={"day": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-#                               "time": ["Lunch", "Dinner"]})
-
-# 	fig = px.bar(df, x="category", y="debit", labels={'debit':'Expenditure'}, hover_data=['category', 'item', 'date', 'debit', 'balance'])
-# 	fig.show()
-
+def categoricalDaysExpenditure(data):
+    start_date , end_date = get_dates()
+    df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
+    df = data.loc[(data['category'] != "FAMILY") & (data['category'] != "SALARY")& (data['category'] != "CREDIT")]
+    fig = [px.bar(df, x="category", y="debit", barmode="group",facet_col="day",text="debit")]
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
 def get_dates(): 
 	start_date ='2020-01-01'
@@ -69,10 +63,6 @@ def get_dates():
 	return start_date , end_date
 
 
-if __name__ == '__main__':
-	main()
-	
-
-
-
-# 22/05/2020	202574	CASH DEPOSIT		UNKNOWN	SELF	0	10,000.00CR	10,000.00CR
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
