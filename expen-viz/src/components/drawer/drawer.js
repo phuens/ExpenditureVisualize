cf15deef483b31afd50916e5a0f8ac5b695e96c4
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2021-01-10 01:08:11 +0600

    ADD: the drawer and grpah layout

--- expen-viz/src/components/drawer/drawer.js
+++ expen-viz/src/components/drawer/drawer.js
@@ -1,6 +1,38 @@
 import React from "react";
-function Drawer() {
-    return "hello world";
+import { makeStyles, withStyles } from "@material-ui/core";
+
+const userStyles = makeStyles({
+    drawer: {
+        display: "flex",
+        flexDirection: "column",
+        position: "absolute",
+        left: "0%",
+        width: "300px",
+        height: "100%",
+        backgroundColor: "red",
+    },
+});
+export default function Drawer() {
+    const classes = userStyles();
+    return <div className={classes.drawer}>sdkjhsfhshf</div>;
 }
 
-export default Drawer;
+// ONE WAY OF PASSING THE STYLES
+// const style = {
+//     drawer: {
+//         display: "flex",
+//         flexDirection: "column",
+//         position: "absolute",
+//         left: "0%",
+//         width: "300px",
+//         height: "100%",
+//         backgroundColor: "red",
+//     },
+// };
+
+// const SideMenu = (props) => {
+//     const { classes } = props;
+//     return <div className={classes.drawer}>sdkjhsfhshf</div>;
+// };
+
+// export default withStyles(style)(SideMenu);
--- expen-viz/src/view/dashboard.js
+++ expen-viz/src/view/dashboard.js
@@ -1,7 +1,24 @@
 import React from "react";
 import Drawer from "../components/drawer/drawer";
+import Navbar from "../components/navbar/navbar";
+import { makeStyles } from "@material-ui/core";
+
+// NOTE: look into border-sizing: border-box if the elements are all moved to the right due to the padding that is being used.
+
+const useStyles = makeStyles({
+    appMain: {
+        paddingLeft: "300px",
+        width: "100%",
+    },
+});
 function Dashboard() {
-    return <Drawer />;
+    const classes = useStyles();
+    return (
+        <>
+            <Drawer />
+            <div className={classes.appMain}>this is where the graphs content will be.</div>
+        </>
+    );
 }
 
 export default Dashboard;
