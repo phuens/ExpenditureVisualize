e3b4b621829b2dbdb40d6582c88a40b104d61ba3
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2021-01-14 02:14:22 +0600

    FIX: documentation flake 8 line too long fix

--- expen-viz/api/api.py
+++ expen-viz/api/api.py
@@ -18,6 +18,7 @@ app = Flask(__name__)
 def home():
     """
     Return all the graphs plotted and handle request from front end.
+
     :return: template with all the grpahs.
     """
     if request.method == 'POST':
@@ -38,8 +39,7 @@ def home():
         data, from_date, to_date)
     pie_categorical_sum_expenditure = pieCategoricalSumExpenditure(
         data, from_date, to_date)
-    graphs = (scatter_bar_daily_sum_and_average, pie_days_expenditure_pie_chart,
-              bar_day_based_categorical_expenditure, bar_categorical_sum_expenditure, pie_categorical_sum_expenditure)
+    graphs = (scatter_bar_daily_sum_and_average, pie_days_expenditure_pie_chart, bar_day_based_categorical_expenditure, bar_categorical_sum_expenditure, pie_categorical_sum_expenditure)  # noqa
     graphs = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     return graphs
 
@@ -47,6 +47,7 @@ def home():
 def readData():
     """
     Read clean and create a single csv containing all the transactions.
+
     :return: csv
     """
     final_csv = []
@@ -73,12 +74,13 @@ def readData():
 def createDataFrame(final_csv):
     """
     Create dataframe, convert data type and add day of the week column.
+
     :return: panadas dataframe
     :parameter: csv containing all the transaction.
     """
     data = pd.concat(final_csv, axis=0, sort=False)
     data.columns = ["date", "journal", "action", "to", "category",
-                    "item", "debit", "credit", "balance"]  # set the column headers
+                    "item", "debit", "credit", "balance"]  # set column headers
 
     data["debit"] = data.debit.astype('float64')
     data["credit"] = data.credit.astype('float64')
@@ -96,6 +98,7 @@ def createDataFrame(final_csv):
 def scatterbarDailySumAndAverage(data, from_date, to_date):
     """
     Total sum and average of spending on a day.
+
     :return: bargraph and scatter plot
     :parameter: data, from_date, to_date
     """
@@ -115,7 +118,7 @@ def scatterbarDailySumAndAverage(data, from_date, to_date):
     layout = go.Layout(
         title_text='Sepnding', yaxis=dict(side='left'),
         yaxis2=dict(overlaying='y', anchor='x',),
-        annotations=[go.layout.Annotation(text='* No average = sum', align='left', showarrow=False, xref='paper', yref='paper', y=1.0291, x=1, width=400, bordercolor='gray', borderwidth=1, opacity=0.5)])
+        annotations=[go.layout.Annotation(text='* No average = sum', align='left', showarrow=False, xref='paper', yref='paper', y=1.0291, x=1, width=400, bordercolor='gray', borderwidth=1, opacity=0.5)])  # noqa
     fig = go.Figure(data=data, layout=layout)
     fig.update_layout(legend=dict(
         orientation="h", yanchor="bottom", y=1.025, xanchor="right", x=1))
@@ -127,6 +130,7 @@ def scatterbarDailySumAndAverage(data, from_date, to_date):
 def pieDaysExpenditurePieChart(data, from_date, to_date):
     """
     Total sum of spending based on days of the week.
+
     :return: piechart
     :parameter: data, from_date, to_date
     """
@@ -135,8 +139,7 @@ def pieDaysExpenditurePieChart(data, from_date, to_date):
     start_date, end_date = get_dates(from_date, to_date)
     df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
     # TODO:NEED TO REFACTOR THE BELOW CODE.
-    df = df.loc[(df["category"] != "UNKNOWN") & (df["category"] != "FAMILY") & (df["category"] != "RENT") & (df["category"] != "SALARY") & (df["category"] != "CREDIT") & (
-        df["category"] != "COMPUTER MONITOR") & (df["category"] != "FURNITURE") & (df["category"] != "RETURN") & (df["category"] != "HALF YR.ANNUAL MAINT CHR") & (df["category"] != "INTEREST")]
+    df = df.loc[(df["category"] != "UNKNOWN") & (df["category"] != "FAMILY") & (df["category"] != "RENT") & (df["category"] != "SALARY") & (df["category"] != "CREDIT") & (df["category"] != "COMPUTER MONITOR") & (df["category"] != "FURNITURE") & (df["category"] != "RETURN") & (df["category"] != "HALF YR.ANNUAL MAINT CHR") & (df["category"] != "INTEREST")]  # noqa
     df = df.groupby('day').debit.agg([sum, len]).reindex(day_of_the_week)
     df["day_of_week"] = df.index
     fig = px.pie(df, values='sum', names='day_of_week', labels="day_of_week",
@@ -150,15 +153,15 @@ def pieDaysExpenditurePieChart(data, from_date, to_date):
 def barDayBasedCategoricalExpenditure(data, from_date, to_date):
     """
     Spending based on Days of the week by categories.
+
     :return: bargraph
     :parameter: data, from_date, to_date
     """
     start_date, end_date = get_dates(from_date, to_date)
     df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
-    df = df.loc[(df["category"] != "UNKNOWN") & (df["category"] != "FAMILY") & (df["category"] != "RENT") & (df["category"] != "SALARY") & (df["category"] != "CREDIT") & (
-        df["category"] != "COMPUTER MONITOR") & (df["category"] != "FURNITURE") & (df["category"] != "RETURN") & (df["category"] != "HALF YR.ANNUAL MAINT CHR") & (df["category"] != "INTEREST")]
+    df = df.loc[(df["category"] != "UNKNOWN") & (df["category"] != "FAMILY") & (df["category"] != "RENT") & (df["category"] != "SALARY") & (df["category"] != "CREDIT") & (df["category"] != "COMPUTER MONITOR") & (df["category"] != "FURNITURE") & (df["category"] != "RETURN") & (df["category"] != "HALF YR.ANNUAL MAINT CHR") & (df["category"] != "INTEREST")]  # noqa
     fig = px.bar(df, x="category", y="debit", barmode="group", facet_col="day",
-                 text="debit", title="Spending based on Days of the week by categories")
+                 text="debit", title="Spending based on Days of the week by categories")  # noqa
     # fig.update_layout(paper_bgcolor="rgba(0,0,0,0)")
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
@@ -168,13 +171,13 @@ def barDayBasedCategoricalExpenditure(data, from_date, to_date):
 def barCategoricalSumExpenditure(data, from_date, to_date):
     """
     Total sum of spending based on Categories.
+
     :return: bargraph
     :parameter: data, from_date, to_date
     """
     start_date, end_date = get_dates(from_date, to_date)
     df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
-    fig = px.bar(df, x="category", y="debit", labels={'debit': 'Expenditure'}, hover_data=[
-                 'category', 'item', 'date', 'debit', 'balance'])
+    fig = px.bar(df, x="category", y="debit", labels={'debit': 'Expenditure'}, hover_data=['category', 'item', 'date', 'debit', 'balance'])  # noqa
     # fig.update_layout(paper_bgcolor="rgba(0,0,0,0)")
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
@@ -184,14 +187,14 @@ def barCategoricalSumExpenditure(data, from_date, to_date):
 def pieCategoricalSumExpenditure(data, from_date, to_date):
     """
     Total sum of spending based on categories.
+
     :return: piechart
     :parameter: data, from_date, to_date
     """
     start_date, end_date = get_dates(from_date, to_date)
     df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
     # TODO:NEED TO REFACTOR THE BELOW CODE.
-    df = df.loc[(df["category"] != "UNKNOWN") & (df["category"] != "FAMILY") & (df["category"] != "RENT") & (df["category"] != "SALARY") & (df["category"] != "CREDIT") & (
-        df["category"] != "COMPUTER MONITOR") & (df["category"] != "FURNITURE") & (df["category"] != "RETURN") & (df["category"] != "HALF YR.ANNUAL MAINT CHR") & (df["category"] != "INTEREST")]
+    df = df.loc[(df["category"] != "UNKNOWN") & (df["category"] != "FAMILY") & (df["category"] != "RENT") & (df["category"] != "SALARY") & (df["category"] != "CREDIT") & (df["category"] != "COMPUTER MONITOR") & (df["category"] != "FURNITURE") & (df["category"] != "RETURN") & (df["category"] != "HALF YR.ANNUAL MAINT CHR") & (df["category"] != "INTEREST")]  # noqa
     df = df.groupby('category').debit.agg([sum, len])
     df["category_group"] = df.index
     fig = px.pie(df, values='sum', names='category_group',
@@ -206,13 +209,13 @@ def pieCategoricalSumExpenditure(data, from_date, to_date):
 def scatterDailyExpenditure(data, from_date, to_date):
     """
     Every single spending.
+
     :return: scatter plot
     :parameter: data, from_date, to_date
     """
     start_date, end_date = get_dates(from_date, to_date)
     df = data.loc[(data['date'] > start_date) & (data['date'] <= end_date)]
-    fig = px.scatter(df, x="date", y="debit", labels={'debit': 'Expenditure'}, hover_data=[
-                     'category', 'item', 'date', 'debit', 'balance'])
+    fig = px.scatter(df, x="date", y="debit", labels={'debit': 'Expenditure'}, hover_data=['category', 'item', 'date', 'debit', 'balance'])  # noqa
     # fig.update_layout(paper_bgcolor="rgba(0,0,0,0)")
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
@@ -221,6 +224,7 @@ def scatterDailyExpenditure(data, from_date, to_date):
 def get_dates(from_date, to_date):
     """
     Convert string to date type.
+
     :return: date
     :parameter: from_date, to_date
     """
